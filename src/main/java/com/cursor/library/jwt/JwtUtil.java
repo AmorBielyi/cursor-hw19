ad8ffb45d39089f84c6bb4bed31897685d8643d7
Roman Bielyi <romanbielyi.amor@gmail.com>
2021-02-22 18:17:51 +0200

    Change secret

--- src/main/java/com/cursor/library/jwt/JwtUtil.java
+++ src/main/java/com/cursor/library/jwt/JwtUtil.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String SECRET_KEY = "secret";
+    private String SECRET_KEY = "dfdgdgd4sfdshfdhgkdhgfjdhfghukjdjfghdjhgkdhgois";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
